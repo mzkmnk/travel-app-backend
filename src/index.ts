7a4ea1522cab5937a3dcb8c174de16d6212500e4
mzkmnk <sma0729mex@gmail.com>
2024-11-30 22:29:16 +0900

    fix:port

--- src/index.ts
+++ src/index.ts
@@ -29,7 +29,7 @@ app.post('/user/create',async ({body}) =>{
   })
 });
 
-app.listen(3000);
+app.listen(process.env.PORT || 3000);
 
 console.log(
   `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
